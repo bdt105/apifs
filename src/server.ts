18a33fb376dffa9eb9a92f3d97c5802bc7219010
bdt105@gmail.com <bdt105@gmail.com>
2018-04-21 20:01:03 +0200

    .

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,8 @@ let bodyParser = require('body-parser');
 let multer = require('multer');
 let upload = multer();
 
-app.use(bodyParser());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Website you wish to allow to connect
