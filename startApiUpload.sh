ffd31f1d1a08c5c74c0e9a5335a2f7490d6452a0
bdt105@gmail.com <bdt105@gmail.com>
2018-07-29 13:40:47 +0100

    .

--- startApiUpload.sh
+++ startApiUpload.sh
@@ -1,2 +1,3 @@
-cd ./build
+ps -ef | grep 'uploadServer.js' | grep -v grep | awk '{print $2}' | xargs kill 1> killLog.out 2> killErr.out
+cd ./build cd ./build
 node uploadServer.js 1> logUpload.out 2> errUpload.out &
