6a9d2e8ad0e57875110ab0a20e4261a0e2c9e6b3
bdt105@gmail.com <bdt105@gmail.com>
2018-07-29 15:15:55 +0100

    .

--- build/testGoogle.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const google_1 = require("./google");
-var g = new google_1.Google();
-//# sourceMappingURL=testGoogle.js.map
