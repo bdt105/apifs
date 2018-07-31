81b146c2f110791743b8ee4e67c76aff39d5af93
bdt105@gmail.com <bdt105@gmail.com>
2018-07-31 20:27:21 +0100

    .

--- startApiFileSystem.sh
+++ startApiFileSystem.sh
@@ -1,2 +1,3 @@
+ps -ef | grep 'apiServer.js' | grep -v grep | awk '{print $2}' | xargs kill -9 1> killLog.out 2> killErr.out
 cd ./build
 node apiServer.js 1> logFs.out 2> errFs.out &
--- startApiUpload.sh
+++ startApiUpload.sh
@@ -1,3 +1,3 @@
-ps -ef | grep 'uploadServer.js' | grep -v grep | awk '{print $2}' | xargs kill 1> killLog.out 2> killErr.out
+ps -ef | grep 'uploadServer.js' | grep -v grep | awk '{print $2}' | xargs kill -9 1> killLog.out 2> killErr.out
 cd ./build cd ./build
 node uploadServer.js 1> logUpload.out 2> errUpload.out &
